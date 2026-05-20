eb1c959153ae54fecbf122749b17f866e976054b
Yash Chauhan <chauhanyash@bugraptors.com>
2026-05-20 16:58:20 +0530

    Authentication Learning

---
+++ tests/TC_021_StorageStateLogin.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require('@playwright/test');
+
+test.use({ storageState: 'utils/user-session.json' });
+
+test('Login using Stored Session', async ({ page }) => {
+
+        // Open Inventory Page without Login
+
+        await page.goto('https://www.saucedemo.com/inventory.html');
+
+        // Validation
+
+        await expect(page).toHaveURL(/inventory/);
+
+        await expect(page.locator('.title')).toHaveText(
+            'Products'
+        );
+
+        console.log('Session Reused Successfully');
+
+    });
---
+++ utils/login-setup.js
@@ -0,0 +1,41 @@
+const { chromium } = require('@playwright/test');
+
+(async () => {
+
+  // =========================
+  // Launch Browser
+  // =========================
+
+  const browser = await chromium.launch({
+      headless: false
+    });
+
+  // =========================
+  // Create Context
+  // =========================
+
+  const context = await browser.newContext();
+
+  const page = await context.newPage();
+
+  await page.goto('https://www.saucedemo.com');
+
+  // Login
+
+  await page.fill('#user-name','standard_user');
+
+  await page.fill('#password','secret_sauce');
+
+  await page.click('#login-button');
+
+  // Wait for Login
+
+  await page.waitForURL('**/inventory.html');
+ 
+  // Save Session
+  await context.storageState({
+    path:'utils/user-session.json'
+  });
+
+  await browser.close();
+})();
